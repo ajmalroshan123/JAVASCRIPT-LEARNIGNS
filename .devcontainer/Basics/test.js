c617289558b0d810e7e4044805617119f4833717
Ajmal Roshan n <119650990+ajmalroshan123@users.noreply.github.com>
2024-07-12 10:14:48 +0000

    second

--- .devcontainer/Basics/test.js
+++ .devcontainer/Basics/test.js
@@ -1,1 +1,10 @@
-console.log("Ajmal Roshan")
+const accountId = 1221
+let accountEmail = "ajmalroshan2002@gmail.com"
+var accountPassword  = 1231223
+
+// accountId = 2
+accountPassword = "roshan2232@gmailcom"
+
+console.log(accountPassword);
+
+console.table([accountEmail,accountId,accountPassword])
